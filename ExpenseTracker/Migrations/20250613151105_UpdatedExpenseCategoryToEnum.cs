6ab70f67fa3e8b36280aebcc860d366dfb1c5064
GOSIjnr <gosijnr7@yahoo.com>
2025-06-13 16:15:19 +0100

    made migrations to accommodate the changes

---
+++ ExpenseTracker/Migrations/20250613151105_UpdatedExpenseCategoryToEnum.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedExpenseCategoryToEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateRecorded",
+                table: "Expenses",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP(6)",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Expenses",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateRecorded",
+                table: "Expenses",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP(6)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Amount",
+                table: "Expenses",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
--- ExpenseTracker/Models/Expense.cs
+++ ExpenseTracker/Models/Expense.cs
@@ -11,8 +11,8 @@ public class Expense
     public DateTime DateRecorded { get; set; } // Auto-filled by backend
 
     [Required(ErrorMessage = "The date of expense must be filled")]
-    public DateTime? DateOfExpense { get; set; } // Optional field from user
+    public DateTime DateOfExpense { get; set; } // Optional field from user
 
-    public string? PaymentMethod { get; set; }
+    [Required] public string PaymentMethod { get; set; } = string.Empty;
     public string? Description { get; set; }
 }
