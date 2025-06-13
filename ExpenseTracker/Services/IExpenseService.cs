e952e270b3e42c73f1811a9181232af233871625
Dubem Egbo <dubem.egbo@saed.dev>
2025-06-13 09:02:47 +0100

    I aded an endpoint that filters the get request by date. I changed and did a migration on dateOfExpense and dateRecorded in the Expense model.

--- ExpenseTracker/Services/IExpenseService.cs
+++ ExpenseTracker/Services/IExpenseService.cs
@@ -5,7 +5,13 @@ namespace ExpenseTracker.Services;
 public interface IExpenseService
 {
     Task<(bool IsSuccess, Expense? Data, string? ErrorMessage)> CreateExpenseAsync(Expense expenseToCreate);
-    Task<Expense?> GetExpenseByIdAsync(Guid id);
+    Task<Expense?> GetExpenseByIdAsync(Guid id);Task<IEnumerable<Expense>> GetFilteredExpensesAsync(
+        DateTime? startDate = null,
+        DateTime? endDate = null,
+        decimal? minAmount = null,
+        decimal? maxAmount = null,
+        decimal? exactAmount = null,
+        string? category = null);
     Task<IEnumerable<Expense>> GetAllExpensesAsync();
     Task<bool> UpdateExpenseAsync(Guid id, Expense expenseToUpdate);
     Task<bool> DeleteExpenseAsync(Guid id);
