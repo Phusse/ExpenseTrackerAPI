40b7184b1bc6565204dbe5886b8d13b21f7cd98f
GOSIjnr <gosijnr7@yahoo.com>
2025-06-13 22:04:05 +0100

    added custom 400 message when sending wrong data

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,6 +1,7 @@
 using System.Text.Json.Serialization;
 using ExpenseTracker.Data;
 using ExpenseTracker.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -24,6 +25,27 @@ builder.Services.AddControllers().AddJsonOptions(options =>
         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     }
 );
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        var errors = context.ModelState
+            .Where(e => e.Value != null && e.Value.Errors.Count > 0)
+            .Select(e => new
+            {
+                Field = e.Key,
+                Errors = e.Value!.Errors.Select(x => x.ErrorMessage)
+            });
+
+        return new BadRequestObjectResult(new
+        {
+            Title = "Validation Failed",
+            Status = 400,
+            Errors = errors
+        });
+    };
+});
 
 WebApplication app = builder.Build();
 
