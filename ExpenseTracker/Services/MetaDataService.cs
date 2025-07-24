7601dbc5065c3402f8a4d6f2e7d504c827c08c8e
GOSIjnr <gosijnr7@yahoo.com>
2025-07-24 21:40:28 +0100

    Add MetaDataService to provide enum

---
+++ ExpenseTracker/Services/MetaDataService.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using ExpenseTracker.Enums;
+using ExpenseTracker.Models.DTOs.MetaData;
+
+namespace ExpenseTracker.Services;
+
+internal class MetaDataService : IEnumService
+{
+	private static List<EnumOptionResponse> GetEnumOptions<T>() where T : Enum
+	{
+		return [.. Enum.GetValues(typeof(T))
+			.Cast<T>()
+			.Select(e => new EnumOptionResponse
+			{
+				Value = e.ToString(),
+				Label = e.GetType()
+						 .GetMember(e.ToString())
+						 .First()
+						 .GetCustomAttribute<DisplayAttribute>()?.Name ?? e.ToString()
+			})];
+	}
+
+	public List<EnumOptionResponse> GetExpenseCategories() => GetEnumOptions<ExpenseCategory>();
+	public List<EnumOptionResponse> GetPaymentMethods() => GetEnumOptions<PaymentMethod>();
+	public List<EnumOptionResponse> GetSavingGoalStatuses() => GetEnumOptions<SavingGoalStatus>();
+}
